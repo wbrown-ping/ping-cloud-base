bbfb4e5e09cf0bdfcd78fe766bb7e372b8c74f10
Walker <walkerbrown@pingidentity.com>
2023-05-26 13:46:45 -0600

    Troubleshooting clone

--- deploy.sh
+++ deploy.sh
@@ -34,7 +34,7 @@ if [[ ${PROJECT_DIR} == *"ping-cloud-base"* ]]; then
   mkdir /tmp/repos
   git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.corp.pingidentity.com/ping-cloud-private-tenant/ping-cloud-common.git /tmp/repos
   export PCC_PATH="/tmp/repos/ping-cloud-common"
-
+  ls -al /tmp/repos/ping-cloud-common/pingcloud-scripts/k8s_utils
   source "${CI_SCRIPTS_DIR}/k8s/deploy/dev_cde_aliases_cicd_config.sh"
   source "${CI_SCRIPTS_DIR}/k8s/deploy/dev_cde_aliases.sh"
 
