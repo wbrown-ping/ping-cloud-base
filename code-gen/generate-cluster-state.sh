2f8d4aae1d5a0134da9d7add1eed9136774dc5ca
Walker <walkerbrown@pingidentity.com>
2023-06-02 10:38:58 -0600

    Debug log

--- code-gen/generate-cluster-state.sh
+++ code-gen/generate-cluster-state.sh
@@ -546,6 +546,7 @@ get_is_ga_variable() {
 #   ${1} -> The value of the IS_MY_PING flag.
 ########################################################################################################################
 get_is_myping_variable() {
+  echo "here, is my ping is ${IS_MY_PING}"
   if test "${IS_MY_PING}" = 'true' || test "${IS_MY_PING}" = 'false'; then
     echo "here, is my ping is ${IS_MY_PING}"
     export IS_MY_PING="${IS_MY_PING}"
