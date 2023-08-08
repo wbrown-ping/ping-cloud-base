f8370e92842029563288fce85dc1891bf1d3e2a9
Walker <walkerbrown@pingidentity.com>
2023-08-08 09:42:34 -0600

    Fixing formatting of skip tests var

--- tests/skip_tests_standard.sh
+++ tests/skip_tests_standard.sh
@@ -1,4 +1,4 @@
 # This file will be sourced during CI/CD execution, and appended to any BRANCH_SKIP_TESTS variables that have been previously declared.
 # It is intended for 'standard' (non customer-hub) deploys
-pingcentral/prerequisites/00-test-urls.sh
-pingcentral/prerequisites/01-verify-pods.sh
+export SCHEDULE_SKIP_TESTS="pingcentral/prerequisites/00-test-urls.sh
+pingcentral/prerequisites/01-verify-pods.sh"
